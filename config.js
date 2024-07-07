a4ad4995a578a4c1b6405fc00aa4693948227d5b
Hamza :) <talkdrove@gmail.com>
2024-07-08 00:21:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,19 +12,19 @@ global.location = 'Earth'
 global.gurl = 'https://instagram.com/talkdrove' // add your username
 global.sudo = process.env.SUDO || "923072380380"
 global.devs = '923072380380';
-global.website = 'HyHamza.vercel.app' //wa.me/+94000000000000
+global.website = 'HyHamza.vercel.app' //wa.me/+92000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/BYTE-MD-LITE.jpeg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWUNTS0hSamM3MVM5aVhaVlU2WE9mTTdxMTVWUlRPQ0FWNDJXVjVuVHoyTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUFkvbEw1blU1UmZyL01Oc0E0WUdqL1BibHQ4YVoyWVdxTFcyTjdicGNIbz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwSXgwZkpqNjJ0ZXQ4cGxNaGoyNDlUSGEyeTZaWk1kWXBSTFVEK2lpeEUwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrOXczNVVCUE42cTRMUnVDM0tLbHZMcGVRcC9HSTd3TmI0ZkZ0RHBTdGgwPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InVHWG5sbjZyUFM2Q2RLb3NTRXhEZzRzcGNnNFdULzFBWEhpc0lyUmJlR1k9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjU4Qy9rT3JQZFdZanpUcHZ2RWI2N1ZSWnh3c0hWTHN0NlVKcmtZdVhlQUU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib05kUFNHbjB1cVk0N0Jjak1DM0poa0E2Uy9LclFYSTZweHpJa0xyYXJtMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU2U1QTdKNXZ6clZidmRVNE9OU2lmQk43Wm9rYzhocFF5cVpYdmN4NkRnND0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im1DRXJKQ2ovVjhLWnJmYTJrS1lNOHJsSU5DdTIwUHV5UXNMRjNSWjlieDB5T1pLUlppcVYyMFRlNXNUMzFjV2lIVFd5UnNGMTBqS0l6NVFPQjhNUkN3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTU3LCJhZHZTZWNyZXRLZXkiOiJHblBsdCtGTENoVEdFR2NJWUlDMHppaXVoNDBEZnBIaG03ZG9tZmdxa1ZNPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJncGtvbG1PQ1RqU0ZRM09IaXplanh3IiwicGhvbmVJZCI6IjY5YmIzNjVlLWFiODItNDZiYi05YmY4LWRjNTM2MjA2MjZlYyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTNmxiOWdicVlBVlVjWUYxVklPS294aEZJUms9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVUVzaDNSREwrZFVyajNXcDhqUSt1eEQyQ1pnPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlBESjRRTlhZIiwibWUiOnsiaWQiOiI5MjMwNzIzODAzODA6NTVAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoiOm8ifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ002QTlJd0dFT3ZUcTdRR0dDQWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InFiWlI5WHJsOWxScGZvRlpZMkd1aUJEMUtqT2lSc3Z6NUFJNXFqUzRtbk09IiwiYWNjb3VudFNpZ25hdHVyZSI6IjJpc0YwMGE0QkQrMWZOZVpVV3VhWG5TWS9PcS9oMWV3WXVUTno4a2I5b2pBWHROSzFKSGdaUU0rRE9LWGZMVTJwelRSMjNJeDlWVnRvYmNqdkVSekF3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJOQ0lwaFIwRnJDTDE5dTJqNzBVNVFZdFpjdjA5SDFPNHdVVzRFVzlHcDNSTmh2bThpRS9FTkNiRGxuUVhlZEZIL1R1YkFiSWVPSkphUjN2WHhZQjJCdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzA3MjM4MDM4MDo1NUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJhbTJVZlY2NWZaVWFYNkJXV05ocm9nUTlTb3pva2JMOCtRQ09hbzB1SnB6In19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwMzc5ODg2fQ==",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'BYTE-MD',
-  packname:  process.env.PACK_NAME || 'MADE BY TALKDROVE',
+  packname:  process.env.PACK_NAME || 'POWERED BY TALKDROVE',
   
   botname:   process.env.BOT_NAME === undefined ? "BYTE-MD" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'TalkDrove' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
-  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
+  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'love' : process.env.ANTI_BAD_WORD,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
