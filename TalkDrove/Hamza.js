bca555aba8512fe5bd3007c4155b44fcf83b9c9b
Hamza :) <talkdrove@gmail.com>
2024-07-16 22:44:59 +0500

    Update Hamza.js 🤍

--- TalkDrove/Hamza.js
+++ TalkDrove/Hamza.js
@@ -1,17 +1,26 @@
-
 var tabCmds = [];
 let cm = [];
 function Hamza(obj, fonctions) {
-    let infoComs = obj;
-    if (!obj.categorie) {
-        infoComs.categorie = "General";
-    }
-    if (!obj.reaction) {
-        infoComs.reaction = "☑️";
-    }
-    infoComs.fonction = fonctions;
-    cm.push(infoComs);
-    // console.log('chargement...')
-    return infoComs;
+  let infoComs = obj;
+  if (obj.category) {
+    obj.categorie = obj.category;
+  }
+  if (!obj.categorie) {
+    infoComs.categorie = "General";
+  }
+  if (!obj.reaction) {
+    infoComs.reaction = "✅";
+  }
+  if (!obj.alias) {
+    obj.alias = [];
+  }
+  infoComs.fonction = fonctions;
+  cm.push(infoComs);
+  // console.log('chargement...')
+  return infoComs;
 }
-module.exports = { Hamza, Module: Hamza, cm };
+module.exports = {
+  zokou,
+  Module: Hamza,
+  cm
+};
