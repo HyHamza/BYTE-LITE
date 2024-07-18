5c615c8759ddc1f348fd606e2133016fed5dcee1
Hamza :) <talkdrove@gmail.com>
2024-07-18 14:54:14 +0500

    B Y T E - L I T E - V E R S I O N  | |  🐼

--- commandes/heroku.js
+++ commandes/heroku.js
@@ -1,88 +1,452 @@
-const { Hamza } = require('../TalkDrove/Hamza');
-const s = require('../set')
+//TalkDrove
 
 
-Hamza(
-    {
-        nomCom : "setvar",
-        categorie : "TalkDrove-Vars"
-    }, async (dest , zk , commandeOptions) =>{
-
-       const {ms,repondre,superUser , arg} = commandeOptions ;
-       
-       if(!superUser){repondre('only Mods can use this commande');return};
-       if(!arg[0] || !(arg.join('').split('='))) {repondre('Bad format ; Exemple of using :\nSetvar OWNER_NAME=Beltah');return};
-     
-    const text = arg.join(" ")
-     const Heroku = require("heroku-client");
-   
-     const heroku = new Heroku({
-        token: s.HEROKU_APY_KEY,
-      });
-
-     let baseURI = "/apps/" + s.HEROKU_APP_NAME;
-        await heroku.patch(baseURI + "/config-vars", {
-          body: {
-                  [text.split('=')[0]]: text.split('=')[1],
-          },
-        });
-        await repondre('Heroku var changes , rebootings....')
-    }
-);
-
-Hamza(
-    {
-        nomCom : "getallvar",
-        categorie : "TalkDrove-Vars"
-    }, async (dest , zk , commandeOptions) =>{
-
-       const {ms,repondre,superUser , arg} = commandeOptions ;
-       
-       if(!superUser){repondre('only mods can use this commande');return}; 
-      
-            const Heroku = require("heroku-client");
-
-			const heroku = new Heroku({
-				token: s.HEROKU_APY_KEY,
-			});
-			let baseURI = "/apps/" + s.HEROKU_APP_NAME;
-
-            let h = await heroku.get(baseURI+'/config-vars')
-let str = '*TalkDrove-Vll vars*\n\n'
-for (vr in h) {
-str+= '☉ *'+vr+'* '+'= '+h[vr]+'\n'
-}
- repondre(str)
 
 
-}
-
-);       
 
 
-    Hamza(
-        {
-            nomCom : "getvar",
-            categorie : "TalkDrove-Vars"
-        }, async (dest , zk , commandeOptions) =>{
-    
-           const {ms,repondre,superUser , arg} = commandeOptions ;
-           
-           if(!superUser){repondre('Only Mods can use this command');return}; 
-           if(!arg[0]) {repondre('insert the variable name in capital letter'); return} ;
-      
-           try {
-            const Heroku = require("heroku-client");
-               
-            const heroku = new Heroku({
-              token: s.HEROKU_APY_KEY,
-            });
-            let baseURI = "/apps/" + s.HEROKU_APP_NAME;
-        let h = await heroku.get(baseURI+'/config-vars')
-        for (vr in h) {
-        if( arg.join(' ') ===vr ) return  repondre( vr+'= '+h[vr]) 	;
-        } 
-        
-        } catch(e) {repondre('Error' + e)}
-   
-        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TalkDrove
+const _0x5f34d7=_0x4b99;(function(_0x13ab01,_0x5776f4){const _0x211f8c=_0x4b99,_0x5e1597=_0x13ab01();while(!![]){try{const _0x48c140=parseInt(_0x211f8c(0x196))/0x1+-parseInt(_0x211f8c(0x1a2))/0x2+parseInt(_0x211f8c(0x192))/0x3+parseInt(_0x211f8c(0x1a8))/0x4*(-parseInt(_0x211f8c(0x1aa))/0x5)+-parseInt(_0x211f8c(0x1a1))/0x6+-parseInt(_0x211f8c(0x19c))/0x7+parseInt(_0x211f8c(0x1a6))/0x8;if(_0x48c140===_0x5776f4)break;else _0x5e1597['push'](_0x5e1597['shift']());}catch(_0xcb93b1){_0x5e1597['push'](_0x5e1597['shift']());}}}(_0x28c8,0xeb47a));function _0x4b99(_0x93905a,_0x13c1c3){const _0x28c8ff=_0x28c8();return _0x4b99=function(_0x4b995b,_0x2cb088){_0x4b995b=_0x4b995b-0x192;let _0x2e42b2=_0x28c8ff[_0x4b995b];return _0x2e42b2;},_0x4b99(_0x93905a,_0x13c1c3);}const {Hamza}=require(_0x5f34d7(0x1a0)),s=require('../set');Hamza({'nomCom':_0x5f34d7(0x19f),'categorie':_0x5f34d7(0x1a7)},async(_0x4c75f3,_0x15a21b,_0x4359ac)=>{const _0x30b711=_0x5f34d7,{ms:_0x5da551,repondre:_0x196a48,superUser:_0xf83048,arg:_0x1c2a18}=_0x4359ac;if(!_0xf83048){_0x196a48(_0x30b711(0x194));return;};if(!_0x1c2a18[0x0]||!_0x1c2a18['join']('')[_0x30b711(0x197)]('=')){_0x196a48(_0x30b711(0x1a4));return;};const _0x23fcdd=_0x1c2a18['join']('\x20'),_0x5b0447=require(_0x30b711(0x1ab)),_0xa3ca8a=new _0x5b0447({'token':s[_0x30b711(0x19b)]});let _0x27b469=_0x30b711(0x1a5)+s[_0x30b711(0x198)];await _0xa3ca8a['patch'](_0x27b469+_0x30b711(0x1ae),{'body':{[_0x23fcdd['split']('=')[0x0]]:_0x23fcdd[_0x30b711(0x197)]('=')[0x1]}}),await _0x196a48(_0x30b711(0x199));}),Hamza({'nomCom':_0x5f34d7(0x19a),'categorie':_0x5f34d7(0x1a7)},async(_0x1c2d24,_0xd12ee7,_0x495632)=>{const _0x2525db=_0x5f34d7,{ms:_0x564a82,repondre:_0x14fffc,superUser:_0x1f15fa,arg:_0x2043bd}=_0x495632;if(!_0x1f15fa){_0x14fffc(_0x2525db(0x19e));return;};const _0x380dbb=require(_0x2525db(0x1ab)),_0x5d2086=new _0x380dbb({'token':s[_0x2525db(0x19b)]});let _0xcb6625=_0x2525db(0x1a5)+s['HEROKU_APP_NAME'],_0x1f8b14=await _0x5d2086[_0x2525db(0x1ac)](_0xcb6625+'/config-vars'),_0x3827eb=_0x2525db(0x19d);for(vr in _0x1f8b14){_0x3827eb+=_0x2525db(0x1a3)+vr+'*\x20'+'=\x20'+_0x1f8b14[vr]+'\x0a';}_0x14fffc(_0x3827eb);}),Hamza({'nomCom':'getvar','categorie':_0x5f34d7(0x1a7)},async(_0x14d8a9,_0x4351df,_0x5743d3)=>{const _0x18348d=_0x5f34d7,{ms:_0x20f14a,repondre:_0x4b881b,superUser:_0x45534b,arg:_0x4e2970}=_0x5743d3;if(!_0x45534b){_0x4b881b(_0x18348d(0x1ad));return;};if(!_0x4e2970[0x0]){_0x4b881b(_0x18348d(0x195));return;};try{const _0x157040=require('heroku-client'),_0x1331bf=new _0x157040({'token':s[_0x18348d(0x19b)]});let _0x1ef1a1=_0x18348d(0x1a5)+s[_0x18348d(0x198)],_0x1b83ef=await _0x1331bf[_0x18348d(0x1ac)](_0x1ef1a1+'/config-vars');for(vr in _0x1b83ef){if(_0x4e2970[_0x18348d(0x1a9)]('\x20')===vr)return _0x4b881b(vr+'=\x20'+_0x1b83ef[vr]);}}catch(_0x46ecd0){_0x4b881b(_0x18348d(0x193)+_0x46ecd0);}});function _0x28c8(){const _0x32cac5=['HEROKU_APP_NAME','Heroku\x20var\x20changes\x20,\x20rebootings....','getallvar','HEROKU_APY_KEY','5507761ozYYOc','*TalkDrove-Vll\x20vars*\x0a\x0a','only\x20mods\x20can\x20use\x20this\x20commande','setvar','../TalkDrove/Hamza','6417714FANFod','501668ZYbbWo','☉\x20*','Bad\x20format\x20;\x20Exemple\x20of\x20using\x20:\x0aSetvar\x20OWNER_NAME=Beltah','/apps/','13100792MoKlbH','Byte-vars','8qWyVui','join','2223365OECpMO','heroku-client','get','Only\x20Mods\x20can\x20use\x20this\x20command','/config-vars','4532490RelbTq','Error','only\x20Mods\x20can\x20use\x20this\x20commande','insert\x20the\x20variable\x20name\x20in\x20capital\x20letter','811899eMOOTP','split'];_0x28c8=function(){return _0x32cac5;};return _0x28c8();}
