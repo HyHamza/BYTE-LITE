c0c7c22903d9399ccadf81c38c3d2295904f72f4
Hamza :) <talkdrove@gmail.com>
2024-07-31 19:05:35 +0500

    oooooooooooooooooof

--- set.js
+++ set.js
@@ -8,8 +8,24 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
+
+
+///////////////////
+
+
 module.exports = { session: process.env.SESSION_ID || 'Byte;;;',
+
+////////////////////////////////
+
+
+
     PREFIXE: process.env.PREFIX || ".",
+
+
+
+///////////////////////////
+    A_REACT : process.env.AUTO_REACTION || 'on',
+    CHATBOT: process.env.CHAT_BOT || "off",
     OWNER_NAME: process.env.OWNER_NAME || "TALKDROVE",
     NUMERO_OWNER : process.env.OWNER_NUMBER || "923072380380",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
@@ -29,21 +45,7 @@ module.exports = { session: process.env.SESSION_ID || 'Byte;;;',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
-    /* new Sequelize({
-     dialect: 'sqlite',
-     storage: DATABASE_URL,
-     logging: false,
-})
-: new Sequelize(DATABASE_URL, {
-     dialect: 'postgres',
-     ssl: true,
-     protocol: 'postgres',
-     dialectOptions: {
-         native: true,
-         ssl: { require: true, rejectUnauthorized: false },
-     },
-     logging: false,
-}),*/
+   
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
