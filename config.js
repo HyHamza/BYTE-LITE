242da8322acd9ee2d79c99778f79849f2fdab36e
dommyhist <dommyraymo@gmail.com>
2025-03-26 04:34:43 +0300

    Update config.js
    const fs = require('fs');
    if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
    
    function convertToBool(text, fault = 'true') {
        return text === fault ? true : false;
    }
    
    module.exports = {
        SESSION_ID: process.env.SESSION_ID || "enter your session",
        CAPTION: process.env.CAPTION || "*ᴘᴏᴡᴅᴇʀᴇᴅ ʙʏ ᴛᴀʟᴋᴅʀᴏᴠᴇ*",
        ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
        AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
        READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
        AUTO_TYPING: process.env.AUTO_TYPING || "true",
        CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
        MODE: process.env.MODE || "public",
        AUTO_VOICE: process.env.AUTO_VOICE || "false",
        AUTO_STICKER: process.env.AUTO_STICKER || "false",
        AUTO_REPLY: process.env.AUTO_REPLY || "false",
        ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/UfzyhWN.jpeg",
        ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M BYTE-LITE WHATSAPP BOT 😊♻️",
        ANTI_LINK: process.env.ANTI_LINK || "true",
        ANTI_BAD: process.env.ANTI_BAD || "true",
        PREFIX: process.env.PREFIX || ".",
        FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
        AUTO_REACT: process.env.AUTO_REACT || "true",
        HEART_REACT: process.env.HEART_REACT || "false",
        OWNER_REACT: process.env.OWNER_REACT || "false",
        BOT_NAME: process.env.BOT_NAME || "Dommyhist",
        AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
        AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`𝒚𝒐𝒖𝒓 𝒔𝒕𝒂𝒕𝒖𝒔 𝒔𝒆𝒆𝒏 𝒋𝒖𝒔𝒕 𝒏𝒐𝒘 𝒃𝒚 𝒃𝒚𝒕𝒆-𝒍𝒊𝒕𝒆`",
        OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
    };

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "false",
     OWNER_REACT: process.env.OWNER_REACT || "false",
-    BOT_NAME: process.env.BOT_NAME || "ʙʏᴛᴇ-ʟɪᴛᴇ",
+    BOT_NAME: process.env.BOT_NAME || "Dommyhist",
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
     AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`𝒚𝒐𝒖𝒓 𝒔𝒕𝒂𝒕𝒖𝒔 𝒔𝒆𝒆𝒏 𝒋𝒖𝒔𝒕 𝒏𝒐𝒘 𝒃𝒚 𝒃𝒚𝒕𝒆-𝒍𝒊𝒕𝒆`",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
