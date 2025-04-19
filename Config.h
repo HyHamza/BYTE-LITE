f3b303c0a9b3ced04ca30bece911b1291f8d7a0f
levi196 <mweetwatimothy22@gmail.com>
2025-04-19 08:40:55 +0200

    Create Config.h

---
+++ Config.h
@@ -0,0 +1,33 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+module.exports = {
+  "SESSION_ID": process.env.SESSION_ID || "",
+  "ALIVE_IMG": process.env.ALIVE_IMG || "https://raw.githubusercontent.com/HyHamza/HyHamza/refs/heads/main/Images/logo.jpg",
+  "PREFIX": process.env.PREFIX || ".",
+  "AUTO_READ_STATUS": process.env.AUTO_READ_STATUS === 'true',
+  "MODE": process.env.MODE || "public",
+  "OWNER_NUMBER": process.env.OWNER_NUMBER || "0974152585",
+  "OWNER_NAME": process.env.OWNER_NAME || "jin woo sung",
+  "ALWAYS_ONLINE": process.env.ALWAYS_ONLINE === 'true',
+  "PRESENCE": process.env.PRESENCE || "online",
+  "PRESENCE": process.env.PRESENCE || "online",
+  "OMDB_API_KEY": process.env.OMDB_API_KEY || "76cb7f39",
+  "READ_CMD": process.env.READ_CMD === 'true',
+  "AUTO_VOICE": process.env.AUTO_VOICE === 'true',
+  "AUTO_STICKER": process.env.AUTO_STICKER === 'true',
+  "AUTO_REPLY": process.env.AUTO_REPLY === 'true',
+  "AUTO_REACT": process.env.AUTO_REACT === 'true',
+  "WELCOME": process.env.WELCOME === 'true',
+  "ANTI_BAD": process.env.ANTI_BAD === 'true',
+  "ANTI_LINK": process.env.ANTI_LINK === 'true',
+  "ANTI_GROUP_LINK": process.env.ANTI_GROUP_LINK === 'true',
+  "ANTI_DELETE": process.env.ANTI_DELETE === 'true',
+  "ANTI_VIEW_ONCE": process.env.ANTI_VIEW_ONCE === 'true',
+  "ANTI_BOT": process.env.ANTI_BOT === 'true',
+  "PREMIUM_USERS": process.env.PREMIUM_USERS || "",
+  "PREMIUM_USERS": process.env.PREMIUM_USERS || "",
+  "COUNTRY_BLOCK": process.env.COUNTRY_BLOCK === 'true',
+  "COUNTRY_BLOCK_CODE": process.env.COUNTRY_BLOCK_CODE || "972",
+  "EMOJI": process.env.EMOJI || "🙄",
+  "AUTO_TYPING": process.env.AUTO_TYPING === 'true'
+};
